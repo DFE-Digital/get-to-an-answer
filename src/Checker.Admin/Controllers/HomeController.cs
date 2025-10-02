e7bcf8c5ab76a3aaf058c74fdd8e89f7037f15bf
Rilwan Owolabi <rilforge@rilwans-mbp.home>
2025-10-03 00:31:45 +0100

    Added some page templates

--- src/Checker.Admin/Controllers/HomeController.cs
+++ src/Checker.Admin/Controllers/HomeController.cs
@@ -4,18 +4,49 @@ using Checker.Admin.Models;
 
 namespace Checker.Admin.Controllers;
 
-public class HomeController : Controller
+public class HomeController(ILogger<HomeController> logger) : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+    private readonly ILogger<HomeController> _logger = logger;
 
     public IActionResult Index()
     {
         return View(new ConfigViewModel());
+    }
+
+    [HttpGet("Page/NewQuestionnaire")]
+    public IActionResult CreateQuestionnairePage()
+    {
+        return View("CreateQuestionnaire", new ConfigViewModel());
+    }
+
+    [HttpGet("Page/Questionnaires/{questionnaireId}/Questions")]
+    public IActionResult ManageQuestionsPage(int questionnaireId)
+    {
+        return View("ManageQuestions", new ConfigViewModel());
+    }
+
+    [HttpGet("Page/Questions/{questionId}")]
+    public IActionResult CreateQuestionPage(int questionId)
+    {
+        return View("AddQuestion", new ConfigViewModel());
+    }
+
+    [HttpGet("Page/Questions/{questionId}/Type")]
+    public IActionResult SetQuestionTypePage(int questionId)
+    {
+        return View("SetQuestionType", new ConfigViewModel());
+    }
+
+    [HttpGet("Page/Branches/{branchId}")]
+    public IActionResult AddBranchingPage(int branchId)
+    {
+        return View("AddBranching", new ConfigViewModel());
+    }
+
+    [HttpGet("Page/Questions/{branchId}/Answers")]
+    public IActionResult AddAnswersPage(int branchId)
+    {
+        return View("AddAnswers", new ConfigViewModel());
     }
 
     public IActionResult Privacy()
