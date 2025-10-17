c1beca2b5d658116ce33275464fbcb55fc21304f
imrak <imrankhalidtoor@gmail.com>
2025-10-17 16:30:58 +0100

    getQuestionnaire function added.

--- src/e2e/gtaa.e2etests/src/tests/api/questionnaire.spec.ts
+++ src/e2e/gtaa.e2etests/src/tests/api/questionnaire.spec.ts
@@ -1,9 +1,9 @@
 import {test, expect} from '@playwright/test';
-import {createQuestionnaire} from "../../test-data-seeder/questionnaire-data";
+import {createQuestionnaire, getQuestionnaire} from "../../test-data-seeder/questionnaire-data";
 
 test.describe('POST Create questionnaire api request', () => {
     test('Validate POST create new questionnaire', async ({page, request}) => {
-        
+
         const questionnairePostResponse = await createQuestionnaire(request);
 
         // --- HTTP-level checks ---
@@ -25,6 +25,29 @@ test.describe('POST Create questionnaire api request', () => {
         // --- Basic content sanity ---
         expect(questionnairePostResponse.title.trim().length).toBeGreaterThan(0);
         expect(questionnairePostResponse.description.trim().length).toBeGreaterThan(0);
+
+        //delete test data logic
+    });
+});
+
+test.describe('GET questionnaire api tests', () => {
+    test('Validate GET questionnaire', async ({request}) => {
+        const questionnairePostResponse = await createQuestionnaire(request);
+        const questionnaireId = await questionnairePostResponse.id;
+        
+        const questionnaireGetResponse = await getQuestionnaire(request, questionnaireId);
+
+        //Schema checks
+        expect(questionnaireGetResponse).toHaveProperty('id');
+        expect(questionnaireGetResponse).toHaveProperty('title');
+        expect(questionnaireGetResponse).toHaveProperty('description');
+        
+        //Type sanity checks
+        expect(typeof questionnaireGetResponse.title).toBe('string');
+        expect(typeof questionnaireGetResponse.description).toBe('string');
+
+        //Content sanity
+        expect(questionnaireGetResponse.title.trim().length).toBeGreaterThan(0);
 
         //delete test data logic
     });
