c4a85edfe357f70ca342a8592bdef6471aa83c4a
Zach Hodges <zach.hodges@hippodigital.co.uk>
2025-10-17 15:43:12 +0100

    Enhance database connection parsing and sqlcmd execution in scripts

--- src/Infrastructure/list-dbo-tables.sh
+++ src/Infrastructure/list-dbo-tables.sh
@@ -5,10 +5,16 @@ set -e
 CONN_STR="${DEFAULT_CONNECTION_STRING}"
 
 # Parse connection string
-server=$(echo "$CONN_STR" | sed -n 's/.*Server=\([^;]*\).*/\1/p')
-db=$(echo "$CONN_STR" | sed -n 's/.*Database=\([^;]*\).*/\1/p')
-user=$(echo "$CONN_STR" | sed -n 's/.*User Id=\([^;]*\).*/\1/p')
-password=$(echo "$CONN_STR" | sed -n 's/.*Password=\([^;]*\).*/\1/p')
+server=$(echo "$CONN_STR" | sed -n 's/.*Server=\([^;,]*\).*/\1/p')
+db=$(echo "$CONN_STR" | sed -n 's/.*Database=\([^;,]*\).*/\1/p')
+user=$(echo "$CONN_STR" | sed -n 's/.*User Id=\([^;,]*\).*/\1/p')
+password=$(echo "$CONN_STR" | sed -n 's/.*Password=\([^;,]*\).*/\1/p')
+port=$(echo "$CONN_STR" | sed -n 's/.*Server=[^,]*,\([0-9]*\).*/\1/p')
+
+# Default port to 1433 if not specified
+if [[ -z "$port" ]]; then
+  port=1433
+fi
 
 if [[ -z "$server" || -z "$db" || -z "$user" || -z "$password" ]]; then
   echo "Could not parse connection string."
@@ -18,5 +24,5 @@ fi
 # Query to list tables in dbo schema
 query="SET NOCOUNT ON; SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo';"
 
-# Run sqlcmd with full path
-/opt/mssql-tools18/bin/sqlcmd -S "$server" -d "$db" -U "$user" -P "$password" -Q "$query"
+# Run sqlcmd with full server and port
+/opt/mssql-tools18/bin/sqlcmd -S "tcp:$server,$port" -d "$db" -U "$user" -P "$password" -Q "$query"
