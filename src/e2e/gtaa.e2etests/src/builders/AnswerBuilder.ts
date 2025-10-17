e9b35e54b985604b6bbd5b2c34dc50473bae9543
imrak <imrankhalidtoor@gmail.com>
2025-10-17 10:24:16 +0100

    Builder classes added.

--- src/e2e/gtaa.e2etests/src/builders/AnswerBuilder.ts
+++ src/e2e/gtaa.e2etests/src/builders/AnswerBuilder.ts
@@ -1,1 +1,50 @@
-import type {AnswerModel} from '../models/api-models';
+import type {AnswerModel, DestinationType} from '../models/api-models';
+
+export class AnswerBuilder {
+    private _questionId: string;
+    private _content = 'Default';
+    private _description = 'Default';
+    private _destination = 'Default';
+    private _destinationType: DestinationType = 'page';
+    private _weight: number = 0.0;
+
+    constructor(questionId: string) {
+        this._questionId = questionId;
+    }
+    
+    withContent(content: string) {
+        this._content = content;
+        return this;
+    }
+
+    withDescription(description: string) {
+        this._description = description;
+        return this;
+    }
+
+    withDestination(destination: string) {
+        this._destination = destination;
+        return this;
+    }
+
+    withDestinationType(destinationType: DestinationType) {
+        this._destinationType = destinationType;
+        return this;
+    }
+
+    withWeight(weight: number) {
+        this._weight = weight;
+        return this;
+    }
+
+    build(): AnswerModel {
+        return {
+            QuestionId: this._questionId,
+            Content: this._content,
+            Description: this._description,
+            Destination: this._destination,
+            DestinationType: this._destinationType,
+            Weight: this._weight
+        };
+    }
+}
--- src/e2e/gtaa.e2etests/src/builders/QuestionBuilder.ts
+++ src/e2e/gtaa.e2etests/src/builders/QuestionBuilder.ts
@@ -1,1 +1,36 @@
-import type {QuestionModel} from '../models/api-models';
+import type {QuestionModel, QuestionType} from '../models/api-models';
+
+export class QuestionBuilder {
+    private _questionnaireId: string;
+    private _content = 'Default question';
+    private _description = 'Default description';
+    private _type: QuestionType = 'single';
+
+    constructor(questionnaireId: string) {
+        this._questionnaireId = questionnaireId;
+    }
+    
+    withContent(content: string) {
+        this._content = content;
+        return this;
+    }
+
+    withDescription(description: string) {
+        this._description = description;
+        return this;
+    }
+
+    withType(questionType: QuestionType) {
+        this._type = questionType;
+        return this;
+    }
+
+    build(): QuestionModel {
+        return {
+            QuestionnaireId: this._questionnaireId,
+            Content: this._content,
+            Description: this._description,
+            Type: this._type,
+        };
+    }
+}
