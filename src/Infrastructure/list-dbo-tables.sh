a7e389eb3e70f1a96a890ea4f03a2c785270a5b3
Zach Hodges <zach.hodges@hippodigital.co.uk>
2025-10-17 15:19:34 +0100

    Testing list tables script to ensure web api is creating tables

---
+++ src/Infrastructure/list-dbo-tables.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Script to list all tables in the dbo schema using the connection string from DEFAULT_CONNECTION_STRING
+set -e
+
+CONN_STR="${DEFAULT_CONNECTION_STRING}"
+
+# Parse connection string
+server=$(echo "$CONN_STR" | sed -n 's/.*Server=\([^;]*\).*/\1/p')
+db=$(echo "$CONN_STR" | sed -n 's/.*Database=\([^;]*\).*/\1/p')
+user=$(echo "$CONN_STR" | sed -n 's/.*User Id=\([^;]*\).*/\1/p')
+password=$(echo "$CONN_STR" | sed -n 's/.*Password=\([^;]*\).*/\1/p')
+
+if [[ -z "$server" || -z "$db" || -z "$user" || -z "$password" ]]; then
+  echo "Could not parse connection string."
+  exit 1
+fi
+
+# Query to list tables in dbo schema
+query="SET NOCOUNT ON; SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo';"
+
+# Run sqlcmd
+sqlcmd -S "$server" -d "$db" -U "$user" -P "$password" -Q "$query"
+
