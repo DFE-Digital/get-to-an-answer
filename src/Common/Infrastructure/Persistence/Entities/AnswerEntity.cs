89da76adc5457e6b3ae69ea709b47b7b1d1a32c5
Zach Hodges <zach.hodges@hippodigital.co.uk>
2025-10-30 15:39:25 +0000

    refactor: remove Content length restriction in AnswerEntity

--- src/Common/Infrastructure/Persistence/Entities/AnswerEntity.cs
+++ src/Common/Infrastructure/Persistence/Entities/AnswerEntity.cs
@@ -16,7 +16,6 @@ public class AnswerEntity
     public Guid Id { get; set; }
 
     [Required]
-    [MaxLength(250)]
     public string Content { get; set; }
     
     public string? Description { get; set; }
