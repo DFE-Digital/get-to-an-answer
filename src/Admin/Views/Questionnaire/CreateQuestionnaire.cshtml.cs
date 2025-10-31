e5206c3d6308931e3402ac15a4fd0cd01df899ec
Zach Hodges <zach.hodges@hippodigital.co.uk>
2025-10-31 16:01:39 +0000

    initial files

---
+++ src/Admin/Views/Questionnaire/CreateQuestionnaire.cshtml.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Admin.Views.Questionnaire;
+
+public class CreateQuestionnaire : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+    
+    public void OnPost()
+    {
+        
+    }
+}
