aa072670a6bb6988cb5095ebf46ab0558dc208f4
Junior Owolabi <146109099+JRilforge@users.noreply.github.com>
2025-11-06 18:39:57 +0000

    CARE-1366 Added mock AzureAD sign-in flow for Local/CI end to end test (#48)
    CARE-1366 Updated Azure AD audience variable and renamed Front Door Terraform file.
    
    Co-authored-by: Rilwan Owolabi <rilforge@rilwans-mbp.home>

--- src/Admin/Program.cs
+++ src/Admin/Program.cs
@@ -1,6 +1,10 @@
 using Azure.Monitor.OpenTelemetry.AspNetCore;
+using Common.Local;
 using Common.Logging;
 using Common.Telemetry;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Trace;
 using Serilog;
@@ -14,16 +18,26 @@ if (builderIsLocalEnvironment)
 {
     builder.Configuration
         .AddUserSecrets<Program>(optional: true, reloadOnChange: true);
+    builder.Services.AddMockAzureAdForMvc();
+}
+else
+{
+    builder.Services
+        .AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+        .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"))
+        .EnableTokenAcquisitionToCallDownstreamApi()
+        .AddInMemoryTokenCaches();
 }
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews()
+    .AddMicrosoftIdentityUI();
 
-builder.AddLogging();
+//builder.AddLogging();
 
 var app = builder.Build();
 
-app.UseLogEnrichment();
+//app.UseLogEnrichment();
 
 // Configure the HTTP request pipeline.
 if (!builderIsLocalEnvironment)
@@ -36,7 +50,13 @@ if (!builderIsLocalEnvironment)
 app.UseHttpsRedirection();
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
+
+if (builderIsLocalEnvironment)
+{
+    app.UseMockMvcDevEndpoints();
+}
 
 app.MapStaticAssets();
 
