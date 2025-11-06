10168c7784e1cc938bc015217d0ec25958baa7bf
Junior Owolabi <146109099+JRilforge@users.noreply.github.com>
2025-11-06 10:59:08 +0000

    CARE-1366 Setup the AzureAD and tested against Azure development Api server (#46)
    * Integrated Application Insights with OpenTelemetry and Serilog for improved logging and telemetry across Admin, API, and Frontend projects. Updated infrastructure to securely fetch Application Insights connection strings from Azure Key Vault.
    
    * Added virtual network subnet integration to Azure App Services in Terraform configuration.
    
    * Migrated Azure App Services to Azure Container Apps with environment-specific configurations for API, Admin, and Frontend components in Terraform.
    
    * Added private endpoint, DNS zone, and VNet integration for Azure Key Vault in Terraform configuration.
    
    * Added the correct docker file to the deployment yaml files
    
    * Migrating back to App Service
    
    ---------
    
    Co-authored-by: Rilwan Owolabi <rilforge@rilwans-mbp.home>

--- src/Infrastructure/terraform/network.tf
+++ src/Infrastructure/terraform/network.tf
@@ -50,9 +50,6 @@ resource "azapi_resource" "gettoananswer_main_subnet" {
         {
           service   = "Microsoft.Sql"
           locations = [azurerm_resource_group.gettoananswer-rg.location]
-        },
-        {
-          service = "Microsoft.KeyVault"
         }
       ]
       networkSecurityGroup = {
@@ -98,27 +95,3 @@ resource "azurerm_mssql_virtual_network_rule" "mssql_vnet_rule" {
   server_id = azurerm_mssql_server.gettoananswer_mssql_server.id
   subnet_id = azapi_resource.gettoananswer_main_subnet.id
 }
-
-# API VNet Integration
-# resource "azurerm_app_service_virtual_network_swift_connection" "api_app_vn_conn" {
-#   app_service_id = azurerm_linux_web_app.gettoananswer-api.id
-#   subnet_id      = azapi_resource.gettoananswer_main_subnet.id
-# 
-#   depends_on = [azurerm_linux_web_app.gettoananswer-api, azapi_resource.gettoananswer_main_subnet]
-# }
-# 
-# # Admin VNet Integration
-# resource "azurerm_app_service_virtual_network_swift_connection" "admin_app_vn_conn" {
-#   app_service_id = azurerm_linux_web_app.gettoananswer-admin.id
-#   subnet_id      = azapi_resource.gettoananswer_main_subnet.id
-# 
-#   depends_on = [azurerm_linux_web_app.gettoananswer-admin, azapi_resource.gettoananswer_main_subnet]
-# }
-# 
-# # Website VNet Integration
-# resource "azurerm_app_service_virtual_network_swift_connection" "frontend_app_vn_conn" {
-#   app_service_id = azurerm_linux_web_app.gettoananswer-frontend.id
-#   subnet_id      = azapi_resource.gettoananswer_main_subnet.id
-# 
-#   depends_on = [azurerm_linux_web_app.gettoananswer-frontend, azapi_resource.gettoananswer_main_subnet]
-# }
