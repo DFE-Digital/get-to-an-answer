b7626bb5df76bd2c00451e4ef5be6b2f23e6d627
Zach Hodges <zach.hodges@hippodigital.co.uk>
2025-11-07 09:30:24 +0000

    Add EditQuestionnaire page for editing existing questionnaires

---
+++ src/Admin/Pages/Questionnaire/EditQuestionnaire.cshtml.cs
@@ -0,0 +1,41 @@
+using Admin.Models;
+using Admin.Models.PageModels;
+using Common.Client;
+using Common.Domain.Request.Update;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Admin.Pages.Questionnaire;
+
+public class EditQuestionnaire(IApiClient apiClient, ILogger<EditQuestionnaire> logger) : QuestionnairesPageModel
+{
+    [FromRoute(Name = "questionnaireId")]
+    public Guid QuestionnaireId { get; set; }
+    
+    [BindProperty]
+    public required UpdateQuestionnaireRequestDto UpdateQuestionnaire { get; set; }
+    
+    
+    public async Task<IActionResult> OnPost()
+    {
+        try
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            
+            await apiClient.UpdateQuestionnaireAsync(QuestionnaireId, UpdateQuestionnaire);
+            
+            TempData[nameof(QuestionnaireState)] = JsonConvert.SerializeObject(new QuestionnaireState { JustUpdated = true });
+            
+            return Redirect($"/admin/questionnaires/{QuestionnaireId}/edit");
+
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, "Error creating questionnaire. Error: {EMessage}", e.Message);
+            return RedirectToPage("/Error");
+        }
+    }
+}
