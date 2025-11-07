64a1815d28565756ed3299b5e652def497ed1dd9
imk049 <imrankhalidtoor@gmail.com>
2025-11-07 10:21:13 +0000

    CARE-1501 test prep with updated page objects (#49)
    * New page objects added for admin.
    
    * Page objects files added.
    
    * Page objects progressed.
    
    * Page objects added and updated.
    
    * Question page objects aadded.
    
    * Page objects added for new question and answer table.
    
    * Tables updated based on DOM from alpha branch.
    
    * Updated code based on DOM from alpha branch.
    
    * Few comments updated.
    
    * Added create and edit modes.
    
    * Add an asnwer page object added.
    
    * Contributors page objects added.
    
    * Did page objects refinement.
    
    * Improved comments.
    
    * Publish questionnaire page object added.
    
    * Scripting in progress for adding a questionnaire.
    
    * Page objects and scripting in progress.
    
    * Typo fixed.
    
    * Updated test title.
    
    * New test script in progress.
    
    * More scripts added for create a new questionnaire.
    
    * Finished scripting for adding a new questionnaire.
    
    * Page objects updated and new scripts added for create questionnaire.
    
    * .env.example file added.

--- src/e2e/gtaa.e2etests/src/tests/admin/add-questionnaire.spec.ts
+++ src/e2e/gtaa.e2etests/src/tests/admin/add-questionnaire.spec.ts
@@ -13,7 +13,8 @@ test.describe('Get to an answer create a new questionnaire', () => {
         const username = 'test'; //to be created dynamically
         const password = 'test'; //to be created dynamically
 
-        await doSignIn(page, username, password);
+        viewQuestionnairePage = await doSignIn(page, username, password);
+        await viewQuestionnairePage.clickCreateNewQuestionnaire();
     });
 
     test('Add a new questionnaire successfully and lands on Edit Questionnaire Page', async ({ page }) => {
@@ -28,5 +29,45 @@ test.describe('Get to an answer create a new questionnaire', () => {
     test('Validate presence of elements on add new questionnaire page', async ({page}) => {
         addQuestionnairePage = await goToAddQuestionnairePage(page);
         await addQuestionnairePage.assertPageElements();
+    });
+
+    test('click back to questionnaire link on add new questionnaire page', async ({page}) => {
+        addQuestionnairePage = await AddQuestionnairePage.create(page);
+        await addQuestionnairePage.ClickBackToQuestionnaireLink();
+
+        viewQuestionnairePage = await ViewQuestionnairePage.create(page);
+        await viewQuestionnairePage.expectUrlOnPage();
+        await viewQuestionnairePage.expectQuestionnaireHeadingOnPage();
+    });
+
+    test('Submit a new questionnaire with missing title', async ({page}) => {
+        addQuestionnairePage = await AddQuestionnairePage.create(page);
+        await addQuestionnairePage.enterTitle(''); 
+        await addQuestionnairePage.clickSaveAndContinue();
+        
+        await addQuestionnairePage.validateMissingTitleMessageFlow();
+    });
+
+    test('Submit a new questionnaire with invalid title', async ({page}) => {
+        addQuestionnairePage = await AddQuestionnairePage.create(page);
+        await addQuestionnairePage.enterTitle('@@@+++###~~~');
+        await addQuestionnairePage.clickSaveAndContinue();
+
+        await addQuestionnairePage.validateInvalidTitleMessageFlow();
+    });
+
+    test('Submit a new questionnaire with invalid title to validate aria-describedby', async ({page}) => {
+        addQuestionnairePage = await AddQuestionnairePage.create(page);
+        await addQuestionnairePage.enterTitle('@@@+++###~~~');
+        await addQuestionnairePage.clickSaveAndContinue();
+
+        await addQuestionnairePage.expectTitleAriaDescribedByIncludesHintAndError();
+    });
+
+    test('should prevent double submission for a questionnaire', async ({ page }) => {
+        addQuestionnairePage = new AddQuestionnairePage(page);
+        await addQuestionnairePage.enterTitle('Double click prevention test');
+        
+        await addQuestionnairePage.submitFormAndCheckNoDoubleSubmit();
     });
 });
--- src/e2e/gtaa.e2etests/src/tests/admin/view-questionnaire.spec.ts
+++ src/e2e/gtaa.e2etests/src/tests/admin/view-questionnaire.spec.ts
@@ -1,18 +1,54 @@
 import {test} from "@playwright/test";
 import {ViewQuestionnairePage} from "../../pages/admin/ViewQuestionnairePage";
-import {doSignIn} from "../../helpers/admin-test-helper";
+import {doSignIn, goToAddQuestionnairePage} from "../../helpers/admin-test-helper";
+import {AddQuestionnairePage} from "../../pages/admin/AddQuestionnairePage";
+import {EditQuestionnairePage} from "../../pages/admin/EditQuestionnairePage";
 
 test.describe('Get to an answer views questionnaire', () => {
     let viewQuestionnairePage: ViewQuestionnairePage;
+    let addQuestionnairePage: AddQuestionnairePage;
+    let editQuestionnairePage: EditQuestionnairePage;
 
     test.beforeEach(async ({page}) => {
         const username = 'test'; //to be created dynamically
         const password = 'test'; //to be created dynamically
 
+        // add a new questionnaire by API
         viewQuestionnairePage = await doSignIn(page, username, password);
     });
 
-    test('Verify questionnaires table on view page', async ({page}) => {
+    test('Validate presence of elements on view questionnaire page', async ({page}) => {
+        await viewQuestionnairePage.assertPageElements();
+    });
+
+    test("Header section - H1 and paragraph presence", async () => {
+        await viewQuestionnairePage.verifyHelpUserHeadingVisible();
+        await viewQuestionnairePage.verifyHelpUserDescriptionVisible();
+    });
+
+    test("Create questionnaire CTA navigates to Add page", async ({page}) => {
+        await viewQuestionnairePage.clickCreateNewQuestionnaire();
+        
+        addQuestionnairePage = await AddQuestionnairePage.create(page);
+        await addQuestionnairePage.expectUrlOnPage();
+        await addQuestionnairePage.verifyLabelAndHintPresent();
+    });
+
+    test("Questionnaires table - columns, title link navigates", async ({page}) => {
+        await viewQuestionnairePage.table.verifyHeaders();
+        await viewQuestionnairePage.table.verifyFirstTitleIsLink();
+        
+        await viewQuestionnairePage.table.clickFirstQuestionnaireTitle();
+        editQuestionnairePage = await EditQuestionnairePage.create(page);
+        await editQuestionnairePage.expectUrlOnPage();
+        await editQuestionnairePage.validateHeadingAndStatus();
+    });
+
+    test("Questionnaires table - status as draft", async ({page}) => {
+        await viewQuestionnairePage.table.expectRowPresentByName('pass the unique questionnaire title from api');
+        
+        // based on unique title this will return the status as draft 
+        await viewQuestionnairePage.table.getStatus('pass the unique questionnaire title from api');
         
     });
 });
