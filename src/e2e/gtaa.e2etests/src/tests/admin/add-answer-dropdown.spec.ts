34aec479b89ad6f8e345fceead15b4fd9ddb7a9d
imrak <imrankhalidtoor@gmail.com>
2025-12-08 09:03:32 +0000

    CARE-1453 answers test scripting in progress.

---
+++ src/e2e/gtaa.e2etests/src/tests/admin/add-answer-dropdown.spec.ts
@@ -0,0 +1,144 @@
+import {test} from "@playwright/test";
+import {goToAddAnswerPageByUrl, signIn} from "../../helpers/admin-test-helper";
+import {ViewQuestionPage} from "../../pages/admin/ViewQuestionPage";
+import {PageHeadings, QuestionType} from "../../constants/test-data-constants";
+import {createContent} from "../../test-data-seeder/content-data";
+import {AddAnswerPage} from "../../pages/admin/AddAnswerPage";
+import {JwtHelper} from "../../helpers/JwtHelper";
+import {createQuestionnaire} from "../../test-data-seeder/questionnaire-data";
+import {createQuestion} from "../../test-data-seeder/question-data";
+
+test.describe('Get to an answer add an answer to a question', () => {
+    let token: string;
+    let questionnaireId: string;
+    let question1Id: string;
+    let question1Content: string;
+    let question2Id: string;
+
+    let addAnswerPage: AddAnswerPage;
+
+    test.beforeEach(async ({request, page}) => {
+        token = JwtHelper.NoRecordsToken();
+        const {questionnaire} = await createQuestionnaire(request, token);
+        questionnaireId = questionnaire.id;
+
+        const q1Resp = await createQuestion(request, questionnaireId, token,
+            undefined, QuestionType.DropdownSelect);
+        question1Id = q1Resp.question.id;
+        question1Content = q1Resp.question.content;
+
+        const q2Resp = await createQuestion(request, questionnaireId, token,
+            undefined, QuestionType.DropdownSelect);
+        question2Id = q2Resp.question.id;
+    });
+
+    test("Submit an answer to a single radio question with NextQuestion destination", async ({page}) => {
+        await signIn(page, token);
+        addAnswerPage = await goToAddAnswerPageByUrl(page, questionnaireId, question1Id);
+
+        await addAnswerPage.expectAnswerHeadingOnPage();
+
+        await addAnswerPage.setOptionContent(0, 'First Answer Option');
+        await addAnswerPage.setOptionHint(0, 'This is the first answer hint');
+        await addAnswerPage.chooseDestination(0, 'NextQuestion');
+
+        await addAnswerPage.setOptionContent(1, 'Second Answer Option');
+        await addAnswerPage.setOptionHint(1, 'This is the second answer hint');
+        await addAnswerPage.chooseDestination(1, 'NextQuestion');
+        await addAnswerPage.clickSaveAndContinueButton();
+
+        const viewQuestionPage = await ViewQuestionPage.create(page);
+        await viewQuestionPage.expectQuestionHeadingOnPage(PageHeadings.VIEW_QUESTION_PAGE_HEADING);
+    });
+
+    test("Submit an answer to a single radio question with SpecificQuestion destination", async ({page}) => {
+        await signIn(page, token);
+        addAnswerPage = await goToAddAnswerPageByUrl(page, questionnaireId, question2Id);
+
+        await addAnswerPage.expectAnswerHeadingOnPage();
+
+        await addAnswerPage.setOptionContent(0, 'First Answer Option');
+        await addAnswerPage.setOptionHint(0, 'This is the first answer hint');
+        await addAnswerPage.setSpecificQuestion(0, question1Content);
+
+        await addAnswerPage.setOptionContent(1, 'Second Answer Option');
+        await addAnswerPage.setOptionHint(1, 'This is the second answer hint');
+        await addAnswerPage.setSpecificQuestion(1, question1Content);
+
+        await addAnswerPage.clickSaveAndContinueButton();
+
+        const viewQuestionPage = await ViewQuestionPage.create(page);
+        await viewQuestionPage.expectQuestionHeadingOnPage(PageHeadings.VIEW_QUESTION_PAGE_HEADING);
+    });
+
+    test("Submit an answer to a single radio question with InternalResultsPage destination", async ({
+                                                                                                        request,
+                                                                                                        page
+                                                                                                    }) => {
+        const title = 'Test Content';
+        const apiContentResponse = await createContent(request, {
+            questionnaireId,
+            title,
+            content: 'This is a test content for the start page.',
+            referenceName: 'test-content'
+        }, token)
+
+        await signIn(page, token);
+        addAnswerPage = await goToAddAnswerPageByUrl(page, questionnaireId, question1Id);
+
+        await addAnswerPage.expectAnswerHeadingOnPage();
+
+        await addAnswerPage.setOptionContent(0, 'First Answer Option');
+        await addAnswerPage.setOptionHint(0, 'This is the first answer hint');
+        await addAnswerPage.setInternalLink(0, title);
+
+        await addAnswerPage.setOptionContent(1, 'Second Answer Option');
+        await addAnswerPage.setOptionHint(1, 'This is the second answer hint');
+        await addAnswerPage.setInternalLink(1, title);
+
+        await addAnswerPage.clickSaveAndContinueButton();
+
+        const viewQuestionPage = await ViewQuestionPage.create(page);
+        await viewQuestionPage.expectQuestionHeadingOnPage(PageHeadings.VIEW_QUESTION_PAGE_HEADING);
+    });
+
+    test("Submit an answer to a single radio question with ExternalResultsPage destination", async ({page}) => {
+        await signIn(page, token);
+        addAnswerPage = await goToAddAnswerPageByUrl(page, questionnaireId, question1Id);
+
+        await addAnswerPage.expectAnswerHeadingOnPage();
+        await addAnswerPage.clickAddAnotherOptionButton();
+
+        await addAnswerPage.setOptionContent(0, 'First Answer Option');
+        await addAnswerPage.setOptionHint(0, 'This is the first answer hint');
+        await addAnswerPage.setExternalLink(0, 'https://www.example.com');
+
+        await addAnswerPage.setOptionContent(1, 'Second Answer Option');
+        await addAnswerPage.setOptionHint(1, 'This is the second answer hint');
+        await addAnswerPage.setExternalLink(1, 'https://www.example.com');
+
+        await addAnswerPage.clickSaveAndContinueButton();
+
+        const viewQuestionPage = await ViewQuestionPage.create(page);
+        await viewQuestionPage.expectQuestionHeadingOnPage(PageHeadings.VIEW_QUESTION_PAGE_HEADING);
+    });
+
+    test("Error summary appears on invalid submit with all missing required fields", async ({page, browserName}) => {
+        await signIn(page, token);
+        addAnswerPage = await goToAddAnswerPageByUrl(page, questionnaireId, question1Id);
+
+        await addAnswerPage.expectAnswerHeadingOnPage();
+        await addAnswerPage.clickSaveAndContinueButton();
+        await addAnswerPage.validateMissingAllFieldsErrorMessageSummary(browserName);
+    })
+
+    test("Inline error and styling for missing option content", async ({page}) => {
+        await signIn(page, token);
+        addAnswerPage = await goToAddAnswerPageByUrl(page, questionnaireId, question1Id);
+
+        await addAnswerPage.expectAnswerHeadingOnPage();
+        await addAnswerPage.clickSaveAndContinueButton();
+        await addAnswerPage.validateInlineQuestionContentError(0);
+        await addAnswerPage.validateInlineQuestionContentError(1);
+    })
+})
