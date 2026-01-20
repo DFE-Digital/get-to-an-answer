4ce9a6bf0ebf539f3d9cf26315364dfee5b2d24e
Zach Hodges <zach.hodges@hippodigital.co.uk>
2026-01-20 10:31:50 +0000

    Re-added anonymous authorization attribute

--- src/Admin/Pages/Home/Support.cshtml.cs
+++ src/Admin/Pages/Home/Support.cshtml.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Admin.Pages.Home;
 
+[AllowAnonymous]
 public class Support : PageModel
 {
     public void OnGet()
