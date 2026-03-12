fded5f0cf2defa743edd6e02cdb6274051b17be4
KaraokeStu <stuart.duncan@education.gov.uk>
2026-03-12 16:12:11 +0000

    Add an event handler for sending events through as each page is submitted

--- src/Frontend/AssetSrc/js/gtaa.internal.js
+++ src/Frontend/AssetSrc/js/gtaa.internal.js
@@ -28,7 +28,37 @@ window.addEventListener('load', function (e) {
         page_path: window.location.pathname,
         redirect_to: externalLinkDest?.value
     });
+
+    // Add an event listener for when the form is posted
+    document.getElementById('question-form').addEventListener('submit', function(e) {
+        // Grab the question content (text only)
+        let question = document.getElementById("question-content").innerText;
+
+        // Get the labels for selected checkboxes
+        let selectedCheckboxes = Array.from(document.querySelectorAll('input[type="checkbox"]:checked')).map(x => x.labels[0].textContent.trim());
+
+        // Get the labels for the selected radio
+        let selectedRadios = Array.from(document.querySelectorAll('input[type="radio"]:checked')).map(x => x.labels[0].textContent.trim());
+
+        // Save these to an answers array
+        let answers = [...selectedCheckboxes, ...selectedRadios];
+
+        // Add the dropdown value to the array, if we've got one
+        let selectedDropdown = document.querySelector('select[name="NextStateRequest.SelectedAnswerIds"]');
+        if (selectedDropdown && selectedDropdown.selectedIndex > 0)
+            answers.push(selectedDropdown.options[selectedDropdown.selectedIndex].text.trim());
+
+        let result = {
+            question: question,
+            answers: answers
+        };
+
+        // Send this event to Google Analytics
+        sendGA4EventToParent('page_submit', result);
+
+    })
     
     const h = document.documentElement.offsetHeight;
     parent?.postMessage({ type: 'HEIGHT', h }, '*');
 })
+
